84c679e331941ce9fab73ae04d4286a7ca83b8b8
TechnoPrometheus <technoprometheus@gmail.com>
2017-01-10 16:18:22 +0100

    start project

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <windows.h>
+#include <winuser.h>
+
+using namespace std;
+
+int save (int key_stroke, char *file);
+
+int main()
+{
+    char i;
+
+    while (1)
+        {
+
+        }
+
+    system ("PAUSE");
+    return 0;
+}
+
+/*************************************************************************/
+
+int save (int key_stroke, char *file)
+{
+    return 0;
+}
